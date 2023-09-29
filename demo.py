806b247484420b3b577e8c0cf3692a4e320ab067
swekshasinha <91781902+swekshasinha@users.noreply.github.com>
2023-09-29 10:02:48 -0500

    add files

---
+++ demo.py
@@ -0,0 +1,22 @@
+import openai
+import streamlit as st
+
+openai.api_key = "lbM0MBuNc2cgwsDvLfbGT3BlbkFJ3duz3VtTKw6pHW6r7cYF"
+def generate_response(myprompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt="You are a trained psychiatrist. Answer the prompts accordingly. ###" + myprompt,
+        temperature=.3,
+        max_tokens=1023
+    )
+    return response.choices[0].text.strip()
+
+def main ():
+    st.title("Friendly Chat Therapist")
+    myprompt = st.text_input("I'm Dr Fraiser Crane. I'm listening. How can I help you? \n")
+    # print(generate_response(myprompt))
+    if st.button("Submit"):
+        st.write(generate_response(myprompt))
+
+if __name__ == "__main__":
+    main()
